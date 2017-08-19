d42cb4120d3d12c2e45cb6531d5cf2990d5d7af0
Jesse <jranon@users.noreply.github.com>
2017-08-19 12:10:55 -0700

    Completed get posts with optional term search

--- src/server.js
+++ src/server.js
@@ -5,12 +5,31 @@ const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [];
+const posts = [{title: 'title', contents: 'go away'}, { title: 'hello', contents: 'hi'}, { title: 'another', contents: 'Hello'}];
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
+
+// server.get('/', (req, res) => {
+//   res.send({ message: 'Welcome!' });
+// });
+
+server.get('/posts', (req, res) => {
+  let results = [];
+  if (req.query) {
+    const { term } = req.query;
+    // filter through each post in posts
+    // set a query array to each post whose title and/or body contains term
+    posts.map((post) => {
+      if (post.title.toLowerCase().includes(term) || post.contents.toLowerCase().includes(term)) {
+        results.push(post);
+      }
+    });
+  } else results = posts;
+  res.send({ results });
+});
 
 module.exports = { posts, server };
