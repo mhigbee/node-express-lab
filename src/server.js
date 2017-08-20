58bdfaf8019845346f3ce9c36244726e2463222b
Matt Higbee <mhigbee@hotmail.com>
2017-08-19 18:51:36 -0600

    solution for lab

--- src/server.js
+++ src/server.js
@@ -5,31 +5,91 @@ const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [{title: 'title', contents: 'go away'}, { title: 'hello', contents: 'hi'}, { title: 'another', contents: 'Hello'}];
+let posts = [];
+let id = 1;
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
-// TODO: your code to handle requests
-
-// server.get('/', (req, res) => {
-//   res.send({ message: 'Welcome!' });
-// });
-
 server.get('/posts', (req, res) => {
-  let results = [];
-  if (req.query) {
-    const { term } = req.query;
-    // filter through each post in posts
-    // set a query array to each post whose title and/or body contains term
-    posts.map((post) => {
-      if (post.title.toLowerCase().includes(term) || post.contents.toLowerCase().includes(term)) {
-        results.push(post);
-      }
+  const term = req.query.term;
+  if (!term) return res.send(posts);
+  const filteredPosts = posts.filter((post) => {
+    return post.title.indexOf(term) !== -1 || post.contents.indexOf(term) !== -1;
+  });
+  res.send(filteredPosts);
+});
+
+server.post('/posts', (req, res) => {
+  const { title, contents } = req.body;
+
+  if (!title || !contents) {
+    res.status(STATUS_USER_ERROR);
+    res.send({
+      error: 'You need to supply a title and contents'
     });
-  } else results = posts;
-  res.send({ results });
+    return;
+  }
+
+  const post = {
+    id,
+    title,
+    contents
+  };
+
+  posts.push(post);
+  id++;
+  res.send(post);
+});
+
+server.put('/posts', (req, res) => {
+  const { title, contents } = req.body;
+  if (!req.body.id || !title || !contents) {
+    res.status(STATUS_USER_ERROR);
+    res.send({
+      error: 'You need to provide an id, title, and contents'
+    });
+    return;
+  }
+
+  const post = posts.find(p => p.id === req.body.id);
+  if (!post) {
+    res.status(STATUS_USER_ERROR);
+    res.send({
+      error: 'You need to provide a valid id'
+    });
+    return;
+  }
+
+  post.title = title;
+  post.contents = contents;
+  res.send(post);
+});
+
+server.delete('/posts', (req, res) => {
+  const postId = req.body.id;
+  if (!postId) {
+    res.status(STATUS_USER_ERROR);
+    res.send({
+      error: 'You need to provide a valid id'
+    });
+    return;
+  }
+
+  const post = posts.find(p => p.id === postId);
+  if (!post) {
+    res.status(STATUS_USER_ERROR);
+    res.send({
+      error: `No post has the id: ${postId}`
+    });
+    return;
+  }
+
+  posts = posts.filter(p => p.id !== postId);
+  res.send({
+    success: true
+  });
 });
 
 module.exports = { posts, server };
